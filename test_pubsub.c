5f80a790e99ce327cdd93e6560d9fad4428804b7
WenZheng Li <me@wenzheng.li>
2020-01-12 16:38:40 +0900

    minor rename in test_pubsub.c

--- test_pubsub.c
+++ test_pubsub.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "pubsub.h"
 
-int message_handler_1(const void* message, const void* src_ctx, void* dst_ctx) {
+int message_handler(const void* message, const void* src_ctx, void* dst_ctx) {
     const int* msg = message;
     printf("got msg %d src_ctx %p dst_ctx %p\n", *msg, src_ctx, dst_ctx);
     return 1;
@@ -9,12 +9,12 @@ int message_handler_1(const void* message, const void* src_ctx, void* dst_ctx) {
 
 MessageSubscriber subscribers[10];
 int main() {
-    message_subscribe(subscribers, message_handler_1, (void*) 1);
-    message_subscribe(subscribers, message_handler_1, (void*) 2);
-    message_subscribe(subscribers, message_handler_1, (void*) 3);
+    message_subscribe(subscribers, message_handler, (void*) 1);
+    message_subscribe(subscribers, message_handler, (void*) 2);
+    message_subscribe(subscribers, message_handler, (void*) 3);
     int msg = 5;
     message_publish(subscribers, &msg, (void*) 0xFF);
-    message_unsubscribe(subscribers, message_handler_1, (void*) 2);
+    message_unsubscribe(subscribers, message_handler, (void*) 2);
     msg = 55;
     message_publish(subscribers, &msg, (void*) 0xF);
     return 0;
