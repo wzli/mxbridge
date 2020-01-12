8ec9dbb09730d888dde3593a6f52035e3608d24a
WenZheng Li <me@wenzheng.li>
2020-01-12 14:37:31 +0900

    added src context to pubsub

--- pubsub.h
+++ pubsub.h
@@ -1,6 +1,6 @@
 #pragma once
 
-typedef int (*MessageHandler)(const void* message, void* context);
+typedef int (*MessageHandler)(const void* message, const void* src_ctx, void* dst_ctx);
 
 typedef struct {
     MessageHandler message_handler;
@@ -11,7 +11,7 @@ typedef struct {
 
 static inline int message_subscribe(
         MessageSubscriber* subscribers, MessageHandler handler, void* context) {
-    subscribers[++(LEN(subscribers))] = (MessageSubscriber){handler, context};
+    subscribers[++LEN(subscribers)] = (MessageSubscriber){handler, context};
     return LEN(subscribers);
 }
 
@@ -26,10 +26,11 @@ static inline int message_unsubscribe(
     return -1;
 }
 
-static inline int message_publish(MessageSubscriber* subscribers, const void* message) {
+static inline int message_publish(
+        MessageSubscriber* subscribers, const void* message, const void* src_ctx) {
     int aggregate = 0;
     for (int i = 1; i <= LEN(subscribers); ++i) {
-        aggregate += subscribers[i].message_handler(message, subscribers[i].context);
+        aggregate += subscribers[i].message_handler(message, src_ctx, subscribers[i].context);
     }
     return aggregate;
 }
--- test_pubsub.c
+++ test_pubsub.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "pubsub.h"
 
-int message_handler_1(const void* message, void* context) {
+int message_handler_1(const void* message, const void* src_ctx, void* dst_ctx) {
     const int* msg = message;
-    printf("got msg %d ctx %p\n", *msg, context);
+    printf("got msg %d src_ctx %p dst_ctx %p\n", *msg, src_ctx, dst_ctx);
     return 1;
 };
 
@@ -13,9 +13,9 @@ int main() {
     message_subscribe(subscribers, message_handler_1, (void*) 2);
     message_subscribe(subscribers, message_handler_1, (void*) 3);
     int msg = 5;
-    message_publish(subscribers, &msg);
+    message_publish(subscribers, &msg, (void*) 0xFF);
     message_unsubscribe(subscribers, message_handler_1, (void*) 2);
     msg = 55;
-    message_publish(subscribers, &msg);
+    message_publish(subscribers, &msg, (void*) 0xF);
     return 0;
 }
